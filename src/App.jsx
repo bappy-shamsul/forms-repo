9685250194ecf09d8ffa9ad854e0cd0010591c5e
Yusha <faizan.mashuq@gmail.com>
2024-11-18 22:34:29 +0600

    Reusable Form is added commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
-import Forms from './components/Forms/Forms'
-import Stateful from './components/Stateful/Stateful'
-import RefForm from './components/RefFoem/RefForm'
-import Hooks from './components/Hook/Hooks'
-import Hookks from './components/TestHooks/Hookks'
+// import Forms from './components/Forms/Forms'
+// import Stateful from './components/Stateful/Stateful'
+// import RefForm from './components/RefFoem/RefForm'
+// import Hooks from './components/Hook/Hooks'
+// import Hookks from './components/TestHooks/Hookks'
+import ReusableForm from './components/ReusableForm/ReusableForm'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+
+  const handleSignSubmit = data =>{
+    console.log('sign up', data)
+}
+  const handleUpdateSubmit = data =>{
+    console.log('updated', data)
+}
+
 
   return (
     <>
       <div>
   
-      <h1>Vite + React</h1>
+      <h1>Welcome to Form Factory!!</h1>
       {/* <Forms></Forms> */}
       {/* <Stateful></Stateful> */}
       {/* <RefForm></RefForm> */}
       {/* <Hooks></Hooks> */}
-      <Hookks></Hookks>
+      {/* <Hookks></Hookks> */}
+      <ReusableForm formTitle='Sign Up' handleSubmit={handleSignSubmit}></ReusableForm>
+      <ReusableForm formTitle='Update Profile' submitBtn='Update' handleSubmit={handleUpdateSubmit}></ReusableForm>
     </div>
     </>
   )
---
+++ src/components/ReusableForm/ReusableForm.jsx
@@ -0,0 +1,33 @@
+
+
+const ReusableForm = ({ formTitle, handleSubmit, submitBtn='Submit' }) => {
+    const handleLocalSubmit = e => {
+        e.preventDefault()
+        const data = {
+            name: e.target.name.value,
+            email: e.target.email.value,
+            password: e.target.password.value
+        }
+        handleSubmit(data)
+    }
+    return (
+        <div>
+            <form onSubmit={handleLocalSubmit}>
+                <div>
+                    {/* <h2>Sign Up</h2> */}
+                    <h2>{formTitle}</h2>
+                </div>
+                <input type="text" name="name" placeholder="Name"></input>
+                <br></br>
+                <input type="text" name="email" placeholder="E-mail"></input>
+                <br></br>
+                <input type="password" name="password" placeholder="Password"></input>
+                <br></br>
+                <input type="submit" value={submitBtn}></input>
+                {/* <button type="submit" value={submitBtn}>Submit</button> */}
+            </form>
+        </div>
+    );
+};
+
+export default ReusableForm;
